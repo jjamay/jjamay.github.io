349fe2ca27591796414c8d5d922eb2ab792ea584
Jordan May <jordaanmay@gmail.com>
2017-09-10 23:49:41 -0400

    revamped site

--- js/script.js
+++ js/script.js
@@ -2,43 +2,33 @@
  * Created by J-May on 2016-01-16.
  */
 
-$(document).ready(function() {
+$(function() {
+  var options = {
+    strings: ['Jordan May'],
+    typeSpeed: 100,
+    // showCursor: false,
+    startDelay: 1000,
+    onComplete: function(self) {
+      $('.typed-cursor').css("visibility", "hidden")
+      window.setTimeout(function() {
+        $('.home').animate({
+          height: "80vh"
+        }, 1000)
+      }, 1000)
+    }
+  }
+  var typed = new Typed('.title', options)
 
-     $.getScript("https://maps.googleapis.com/maps/api/js", function () {
-         var mapDiv = document.getElementById('map');
-         var prison = {lat: 43.4722854, lng: -80.5470463};
-         var map = new google.maps.Map(mapDiv, {
-              center: prison,
-              zoom: 13
-         });
-         var marker = new google.maps.Marker({
-              position: prison,
-              map: map
-         })
-     });
-
-     $('#portrait')
-     .click(function() {
-          $('html, body').animate({
-               scrollTop: $('#welcome').offset().top
-          }, 1000)
-     });
-
-     $('.fa-envelope-square')
-     .hover(
-          function() {
-               $('.email').fadeIn(200);
-          },
-          function() {
-               $('.email').fadeOut(200);
-          }
-     );
-
-     $('#resume-pic')
-     .on('hide.bs.collapse', function() {
-          $('#resume-btn').text('Show Resume');
-     })
-     .on('show.bs.collapse', function() {
-          $('#resume-btn').text('Hide Resume');
-     })
-});
+  $.getScript("https://maps.googleapis.com/maps/api/js", function () {
+    var mapDiv = document.getElementById('map')
+    var prison = {lat: 43.4722854, lng: -80.5470463}
+    var map = new google.maps.Map(mapDiv, {
+      center: prison,
+      zoom: 13
+    })
+    var marker = new google.maps.Marker({
+      position: prison,
+      map: map
+    })
+  })
+})
